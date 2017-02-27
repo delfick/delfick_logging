52d434524af4fb59159d11fec96f1695bc1da208
Stephen Moore <stephen@delfick.com>
2017-02-27 21:18:21 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name = "delfick_logging"
-    , version = "0.1"
+    , version = "0.2"
     , py_modules = ['delfick_logging']
 
     , install_requires =
